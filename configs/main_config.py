ff4a435f3100fcc676acb7aeb5c0b766cd1f0dd5
suka1557 <suka1557@gmail.com>
2024-12-08 15:31:01 +0530

    added option to specify mongo uri based on env in main config

--- configs/main_config.py
+++ configs/main_config.py
@@ -13,10 +13,17 @@ news_csv_file_path = os.path.join(PROJECT_ROOT, 'data', 'news.csv')
 load_dotenv()
 
 # Mongo DB details
+ENV = os.getenv("ENV", "local")
 MONGO_DB_HOST = os.getenv("MONGO_HOST", "localhost")
 MONGO_DB_PORT = os.getenv("MONGO_PORT", "27017")
 MONGO_DB_NAME = os.getenv("MONGO_DB_NAME", "news")
-MONGO_URI = f"mongodb://{MONGO_DB_HOST}:{MONGO_DB_PORT}"
+if ENV == "local":
+    MONGO_URI = f"mongodb://{MONGO_DB_HOST}:{MONGO_DB_PORT}"
+    print(f"Mongo URI: {MONGO_URI}")
+else:
+    MONGO_SERVICE_NAME = os.getenv("MONGO_SERVICE_NAME", "mongodb1")
+    MONGO_URI = f"mongodb://{MONGO_SERVICE_NAME}:{MONGO_DB_PORT}"
+    print(f"Mongo URI: {MONGO_URI}")
 
 # MONGO COllection
 FAKE_NEWS_COLLECTION_NAME = 'fake-news'
